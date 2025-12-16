704cf1c4c952c02421daba1300b218830603ac51
Marwane <marwane.seffah@etu.univ-amu.fr>
2025-12-16 11:39:50 +0100

    Ajout du projet qu'on rendra (c'est le squelette)

---
+++ sae_rendu/sae_candy_crush_rendu/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+// Ici on trouvera les fonctions liées au jeu lui même à savoir les actions sur les lignes, colonnes etc
+// à predre depuis le travail de brandon dans le TP 10 (le truc qu'il a commit)
+
+
+// Ici on trouvera les fonctions liées au choix des modes de jeu /  accès au menu
+void lancerModeJcJ () {
+    return;
+}
+
+void lancerModeHistoire () {
+    return;
+}
+
+void lancerModeInfini () {
+    return;
+}
+
+bool testAccesMenuPause () {
+    return false; // mis pour eviter les avertissements à modifier
+}
+
+void accesMenuPause () {
+    return;
+}
+
+void accesMenu () {
+    return;
+}
+
+// ...
+// on ajoutera/modifiera les fonction à chaque fois qu'on en aura besoin
+
+int main()
+{
+    bool jeuEnCours = true;
+    while (jeuEnCours) {
+        // c'est dans cette boucle que le jeu va tourner
+    }
+    // cout << "Hello World!" << endl;
+    return 0;
+}
