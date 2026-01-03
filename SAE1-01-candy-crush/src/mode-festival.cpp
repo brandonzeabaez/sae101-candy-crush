11517a14a5c613de7f38d0fad0bd50f756ccbab6
Tommy Li <tommy.li@etu.univ-amu.fr>
2026-01-03 16:20:48 +0100

    version 2.1 - commentaire et score
    commentaire et score

--- SAE1-01-candy-crush/src/mode-festival.cpp
+++ SAE1-01-candy-crush/src/mode-festival.cpp
@@ -10,7 +10,7 @@ using namespace manipulationDeLaGrille;
 using namespace testSurLaGrille;
 using namespace festival;
 
-
+// verification de mouvement valide pour mode festival
 bool mouvementValideFestival(const CMatrice& grille, int x, int y, char direction) {
     bool cond = true;
     if (x < 0 || x >= grille.size() || y < 0 || y >= grille.size()) {
@@ -26,11 +26,11 @@ bool mouvementValideFestival(const CMatrice& grille, int x, int y, char directio
         if (y >= grille.size() - 1)
             cond = false;
         break;
-    case 'a': // Left
+    case 'a':
         if (x <= 0)
             cond = false;
         break;
-    case 'e': // Right
+    case 'e':
         if (x >= grille.size() - 1)
             cond = false;
         break;
@@ -41,20 +41,58 @@ bool mouvementValideFestival(const CMatrice& grille, int x, int y, char directio
     return cond;
 }
 
+// calcule de score pour mode festival
+int calculeScore(const CMatrice & grille){
+    int score = 0;
+    for (size_t i = 0; i < grille.size(); ++i){
+        for (size_t j = 0; j < grille.size(); ++i){
+            if (grille[i][j] != KImpossible){
+                score = score + 5;
+            }
+        }
+    }
+    return score;
+}
 
 
+// jeu principale
 int modeFestival(){
+    // initialisation des variables
     CMatrice grille;
     size_t taille = 10;
     unsigned combien;
     CPosition pos;
     char direction;
+    int coups = 11;
+    int score = 0;
 
     InitiationGrille(grille, taille);
 
-    while(true){
+    // boucle principale du jeu
+    while(coups != 0){
         afficherLaGrille(grille);
+        cout << "Il reste " << coups << "coups" << endl;
 
+        // verifier si mouvement est valide
+        bool valide = false;
+        while (!valide) {
+            cout << "Donne abscisse (X) : ";
+            cin >> pos.first;
+            cout << "Donne ordonnee (Y) : ";
+            cin >> pos.second;
+            cout << "Donne direction (Z, S, A, E) : ";
+            cin >> direction;
+
+            if (mouvementValideFestival(grille, pos.first, pos.second, direction)) {
+                valide = true;
+            }
+            else {
+                cout << "Coup impossible" << endl;
+            }
+        }
+        faireUnMouvement(grille, pos, direction);
+
+        // enleve les lignes et colonnes avec detection de bombe
         while(auMoinsTroisDansLaLigne(grille, pos, combien))
         {
             int couleur = detectionBombe(grille, pos, combien, true);
@@ -79,21 +117,9 @@ int modeFestival(){
             cout << "------" << endl;
         }
     }
-    bool valide = false;
-    while (!valide) {
-        cout << "Donne abscisse (X) : ";
-        cin >> pos.first;
-        cout << "Donne ordonnee (Y) : ";
-        cin >> pos.second;
-        cout << "Donne direction (Z, S, A, E) : ";
-        cin >> direction;
 
-        if (mouvementValideFestival(grille, pos.first, pos.second, direction)) {
-            valide = true;
-        } else {
-            cout << "Coup impossible" << endl;
-        }
-    }
 
-    faireUnMouvement(grille, pos, direction);
+    score = calculeScore(grille);
+    cout << "Score: " << score << endl;
+    return 0;
 }
